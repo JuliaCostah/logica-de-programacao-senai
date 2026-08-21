1430fdbb1426de293b80815bff59ecded5730086
Júlia Roberta <juliacosta.tech@gmail.com>
2026-08-21 19:02:19 -0300

    Exercìcio 08
    programa que pede ao usuário para inserir dois números e uma operação(adição,subtração,multiplicação ou divisão). Realizando a operação solicitada e exibindo o resultado.

---
+++ lista-01-condicionais/exer_008.py
@@ -0,0 +1,25 @@
+num1 = float(input('Informe um número: '))
+num2 = float(input('Informe um segundo número: '))
+
+print('Operações:\n\tAdição(+)\n\tSubtração(-)\n\tMultiplicação(*)\n\tDivisão(/)')
+operador = input('Escolha uma operação: ')
+
+if operador == '+':
+    calculo = num1 + num2
+    print(f'A soma é {calculo}')
+elif operador == '-':
+    calculo = num1 - num2
+    print(f'A subtração é {calculo}')
+elif operador == '*':
+    calculo = num1 * num2
+    print(f'A multiplicação é {calculo}')
+elif operador == '/':
+    if num2 == 0:
+        print('ímpossivel dividir por zero.')
+    else:    
+        calculo = num1/num2
+        print(f'A divisão é {calculo}')
+else:
+    print(f'Operador inválido.')
+    
+    
