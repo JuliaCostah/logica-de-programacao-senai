34e086ecd0e52f6fd8b8fb28bc079999f33aaf87
Júlia Roberta <juliacosta.tech@gmail.com>
2026-09-08 16:16:03 -0300

    Notas

---
+++ lista-02-loops/ex05.py
@@ -0,0 +1,22 @@
+import os
+notas = []
+
+while True:
+    n = float(input('Notas (-1 para finalizar): R$'))
+    if n == -1:
+        break
+    else:
+        notas.append(n)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+if notas:  
+    print('   Notas Cadastradas')
+    for n in notas:
+        print(f'R$ {n:.2f} | ',end='')
+
+    media = sum(notas)/len(notas)
+    print(f'\nQuantidade: {len(notas)} | Maior nota: {max(notas)} | Menor nota: {min(notas)}')
+    notas.sort(reverse=True)
+    print(f'Notas em ordem decrescente: {notas}')
+else:
+    print('Nenhuma nota cadastrada.')
